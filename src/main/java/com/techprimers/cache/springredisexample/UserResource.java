66dcf362fcc88ebb19df8540974aa7b89ae859e1
zians786 <zian.ansari19@gmail.com>
2018-01-11 19:39:08 +0530

    Springboot with redis

--- src/main/java/com/techprimers/cache/springredisexample/UserResource.java
+++ src/main/java/com/techprimers/cache/springredisexample/UserResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/rest/user")
 public class UserResource {
 
     private UserRepository userRepository;
